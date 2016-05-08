31dab53449553d9080e209dec387a836f118d1c6
Brian Daub <brian@rockridgecreative.com>
2016-05-08 23:37:10 +0000

    added css3 animations to _skills.sass

--- js/skills.js
+++ js/skills.js
@@ -2,56 +2,48 @@ var skills_obj = [
     {
       'id': 'html',
       'title': 'HTML5',
-      'xp': 8,
-      'length': 9
+      'xp': 95,
     },
     {
       'id': 'css',
       'title': 'CSS3',
-      'xp': 7,
-      'length': 9
+      'xp': 90,
     },
     {
       'id': 'bootstrap',
       'title': 'Bootstrap',
-      'xp': 5,
-      'length': 9
+      'xp': 70,
     },
     {
       'id': 'js',
       'title': 'JavaScript',
-      'xp': 6,
-      'length': 9
+      'xp': 75,
     },
     {
       'id': 'jquery',
       'title': 'jQuery',
-      'xp': 6,
-      'length': 9
+      'xp': 75,
     },
     {
       'id': 'git',
       'title': 'Git/GitHub',
-      'xp': 7,
-      'length': 9
+      'xp': 70,
     },
     {
       'id': 'ps',
       'title': 'Photoshop',
-      'xp': 6,
-      'length': 9
+      'xp': 80,
     },
     {
       'id': 'is',
       'title': 'Illustrator',
-      'xp': 4,
-      'length': 9
+      'xp': 60,
     }
 ]
 
 $(document).ready(function () {
     var list = $('.skill-list').html('<ul class="list-group"></ul>').find('ul');
     for(var i = 0; i < skills_obj.length; i++){
-        list.append("<li class='bar-outter' >" + "<div class='bar-inner "  + skills_obj[i].id + "' data-percent='" + skills_obj[i].xp  + "0%' >" +  "</div>" + "</li>");
+        list.append("<li class='bar-outter' >" + "<div class='bar-inner "  + skills_obj[i].id + "' data-percent='" + skills_obj[i].xp  + "%' >" +  "</div>" + "</li>");
     }
 });
