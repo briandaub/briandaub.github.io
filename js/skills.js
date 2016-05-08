9df5c26a307d560719a8a5e7d4ee83d09dbc93fa
Brian Daub <brian@rockridgecreative.com>
2016-05-08 21:59:19 +0000

    changed skills.js to create titles dynamically, changed skills.sass to add title content

--- js/skills.js
+++ js/skills.js
@@ -1,40 +1,48 @@
 var skills_obj = [
     {
+      'id': 'html',
       'title': 'HTML5',
       'xp': 8,
       'length': 9
     },
     {
+      'id': 'css',
       'title': 'CSS3',
       'xp': 7,
       'length': 9
     },
     {
+      'id': 'bootstrap',
       'title': 'Bootstrap',
       'xp': 5,
       'length': 9
     },
     {
+      'id': 'js',
       'title': 'JavaScript',
       'xp': 6,
       'length': 9
     },
     {
+      'id': 'jquery',
       'title': 'jQuery',
       'xp': 6,
       'length': 9
     },
     {
+      'id': 'git',
       'title': 'Git/GitHub',
       'xp': 7,
       'length': 9
     },
     {
+      'id': 'ps',
       'title': 'Photoshop',
       'xp': 6,
       'length': 9
     },
     {
+      'id': 'is',
       'title': 'Illustrator',
       'xp': 4,
       'length': 9
@@ -44,6 +52,6 @@ var skills_obj = [
 $(document).ready(function () {
     var list = $('.skill-list').html('<ul class="list-group"></ul>').find('ul');
     for(var i = 0; i < skills_obj.length; i++){
-        list.append("<h3 class='bar-title'>" + skills_obj[i].title + "</h3>" + "<li class='bar-outter' >" + "<div class='bar-inner' data-percent='" + skills_obj[i].xp  + "0%' >" +  "</div>" + "</li>");  
+        list.append("<li class='bar-outter' >" + "<div class='bar-inner "  + skills_obj[i].id + "' data-percent='" + skills_obj[i].xp  + "0%' >" +  "</div>" + "</li>");
     }
 });
